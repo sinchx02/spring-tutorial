fc2fbc711021aedd5cd166573b0832e9886b25f3
Sinchana <rsinchana45@gmail.com>
2025-04-21 23:41:59 +0530

    Implementation of setter injection

--- src/main/java/com/project/store/OrderService.java
+++ src/main/java/com/project/store/OrderService.java
@@ -2,14 +2,19 @@ package com.project.store;
 
 public class OrderService {
 
-    private PaymentService paymentService;
-    public OrderService(PaymentService paymentService){ //passing the injection in contructor
-        this.paymentService=paymentService;
-    }
 
+    private PaymentService paymentService;
+
+//    public OrderService(PaymentService paymentService){ //passing the injection in contructor
+//        this.paymentService=paymentService;
+//    }
     public void placeOrder()
     {
         paymentService.payment(8900);
+    }
+
+    public void setPaymentService(PaymentService paymentService) {
+        this.paymentService = paymentService;
     }
 
 
--- src/main/java/com/project/store/StoreApplication.java
+++ src/main/java/com/project/store/StoreApplication.java
@@ -9,9 +9,12 @@ public class StoreApplication {
 	public static void main(String[] args) {
 
 		//SpringApplication.run(StoreApplication.class, args);
-		OrderService orderService = new OrderService(new GooglePayPaymentService());
+		OrderService orderService = new OrderService();
+		orderService.setPaymentService(new GooglePayPaymentService());
 		orderService.placeOrder();
-        //constructor injection --> where without changing order service both payment services can be implemented
+        //setter injection --> where without changing order service both payment services can be implemented
+		// This type of injection is only used for optional dependency as it throws null pointer exception if setPaymentService is not called
+		//in this case setter injection is not recommended as paymentService is required and not optional
 	}
 
 }
